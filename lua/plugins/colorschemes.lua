28323682bc672b85bd4ced64314f247efe152993
Abhinav Mishra <abhinavmishra3322@gmail.com>
2024-09-06 09:33:07 +0530

    update

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -56,11 +56,12 @@ return {
     lazy = false,
     priority = 1000,
   },
+  { "rose-pine/neovim", name = "rose-pine" },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "solarized-osaka",
+      colorscheme = "rose-pine",
     },
   },
 }
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,16 +1,17 @@
 -- Register your mappings and descriptions
-require("lazy").setup({
-  {
-    "folke/which-key.nvim",
-    config = function()
-      local wk = require("which-key")
-      wk.setup({})
-      wk.register({
-        r = {
-          name = "Rename",
-          n = { ":IncRename<CR>", "Incremental Rename" },
-        },
-      }, { prefix = "<leader>" })
-    end,
-  },
-})
+-- require("lazy").setup({
+--   {
+--     "folke/which-key.nvim",
+--     config = function()
+--       local wk = require("which-key")
+--       wk.setup({})
+--       wk.register({
+--         r = {
+--           name = "Rename",
+--           n = { ":IncRename<CR>", "Incremental Rename" },
+--         },
+--       }, { prefix = "<leader>" })
+--     end,
+--   },
+-- })
+return {}
